886c4cc4305019b73cffa27c7081fddcc24e666e
Sarah3902 <leroy.sarah39@gmail.com>
2021-09-21 16:54:50 +0200

    carrousel personnages

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,21 @@
 // Carrousel
 
-let suivant = document.querySelector("#bout_D");
-let precedent = document.querySelector("#bout_G");
-let carrousel = document.querySelector(".carrousel_persos");
-
+// Variable globale
 let index = 0;
+let index1 = 0;
 
-suivant.addEventListener("click", () => {
-  index += 1;
-  carrousel.style.transform = `rotateY(${-60 * index}deg)`;
-});
+// Gestion des événements
+$('span').click(function() {
+  // Récupération index
+  const indexN = $('span').index(this);
+  const indexP = $('span').index(this);
 
-precedent.addEventListener("click", () => {
-  index -= 1;
-  carrousel.style.transform = `rotateY(${60 * index}deg)`;
+  // Renouveller l'image
+  $('.car_img').eq(index).fadeOut(1000).end().eq(indexN).fadeIn(1000);
+  // Renouveller le texte
+  $('.infos_persos>p').eq(index1).fadeOut(1000).end().eq(indexP).fadeIn(1000);
+
+  // Mettre à jour l'index
+  index = indexN;
+  index1 = indexP;
 });

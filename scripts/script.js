2b9a09c9b0cd9bd63a52b2935658d57db9189456
Sarah3902 <leroy.sarah39@gmail.com>
2021-09-21 22:48:09 +0200

    pop up

--- scripts/script.js
+++ scripts/script.js
@@ -33,3 +33,17 @@ $('span').click(function() {
 //   index2 = indexN1;
 //   index3 = indexP1;
 });
+
+// POP UP
+let btnPopup = document.getElementById('btnPopup');
+let overlay = document.getElementById('overlay');
+btnPopup.addEventListener('click',openMoadl);
+function openMoadl() {
+overlay.style.display='block';
+}
+
+var btnClose = document.getElementById('btnClose');
+btnClose.addEventListener('click',closeModal);
+function closeModal() {
+  overlay.style.display='none';
+  }

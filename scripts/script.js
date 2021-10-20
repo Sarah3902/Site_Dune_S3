97d1e29bb6ded71fb92b3bcef99d3e4307b6a3fe
Sarah3902 <leroy.sarah39@gmail.com>
2021-10-20 15:37:07 +0200

    carrousel img film

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
-// CARROUSEL
+
+// CARROUSEL PERSONNAGES
 
 // Variable globale
 let index = 0;
 let index1 = 0;
-let index2 = 0;
-let index3 = 0;
+
 
 // Gestion des événements
 $('.control>span').click(function() {
@@ -22,20 +22,62 @@ $('.control>span').click(function() {
   index1 = indexP;
 });
 
-$('.fleches').click(function() {
-  // Récupération index
-  const indexN1 = $('.fleches').index(this);
-  const indexP1 = $('.fleches').index(this);
+$('.avant').click(function() {
 
   // Renouveller l'image
-  $('.car_img').eq(index2).fadeOut(1000).end().eq(indexN1).fadeIn(1000);
+  $('.car_img').eq(index).fadeOut(1000).end().eq(index - 1).fadeIn(1000);
   // Renouveller le texte
-  $('.infos_persos>p').eq(index3).fadeOut(1000).end().eq(indexP1).fadeIn(1000);
+  $('.infos_persos>p').eq(index1).fadeOut(1000).end().eq(index1 - 1).fadeIn(1000);
 
   // Mettre à jour l'index
-  index2 = indexN1;
-  index3 = indexP1;
+  index = index-1;
+  index1 = index1-1;
 });
+
+$('.apres').click(function() {
+
+  // Renouveller l'image
+  $('.car_img').eq(index).fadeOut(1000).end().eq(index + 1).fadeIn(1000);
+  // Renouveller le texte
+  $('.infos_persos>p').eq(index1).fadeOut(1000).end().eq(index1 + 1).fadeIn(1000);
+
+  // Mettre à jour l'index
+  index = index+1;
+  index1 = index1+1;
+});
+
+
+
+// CARROUSEL IMG FILM
+
+let index2 = 0;
+let index3 = 0;
+
+$('.avant2').click(function() {
+
+  // Renouveller l'image
+  $('.img_film').eq(index2).fadeOut(1000).end().eq(index2 - 1).fadeIn(1000);
+  // Renouveller le texte
+  $('.infos_persos>p').eq(index3).fadeOut(1000).end().eq(index3 - 1).fadeIn(1000);
+
+  // Mettre à jour l'index
+  index2 = index2-1;
+  index3 = index3-1;
+});
+
+$('.apres2').click(function() {
+
+  // Renouveller l'image
+  $('.img_film').eq(index2).fadeOut(1000).end().eq(index2 + 1).fadeIn(1000);
+  // Renouveller le texte
+  $('.infos_persos>p').eq(index3).fadeOut(1000).end().eq(index3 + 1).fadeIn(1000);
+
+  // Mettre à jour l'index
+  index2 = index2 +1;
+  index3 = index3 +1;
+});
+
+
 
 // POP UP
 let btnPopup = document.getElementById('btnPopup');

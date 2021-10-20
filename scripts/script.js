4a0c8f8ed68213d26f41fda390c7632dc282bb20
Sarah3902 <leroy.sarah39@gmail.com>
2021-10-20 14:47:10 +0200

    fix bug carrousel persos + fix pop up infos

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// Carrousel
+// CARROUSEL
 
 // Variable globale
 let index = 0;
@@ -7,31 +7,34 @@ let index2 = 0;
 let index3 = 0;
 
 // Gestion des événements
-$('span').click(function() {
+$('.control>span').click(function() {
   // Récupération index
-  const indexN = $('span').index(this);
-  const indexP = $('span').index(this);
-
-// $('.fleches').click(function() {
-//   // Récupération index
-//   const indexN1 = $('.fleches').index(this);
-//   const indexP1 = $('.fleches').index(this); 
+  const indexN = $('.control>span').index(this);
+  const indexP = $('.control>span').index(this);
 
   // Renouveller l'image
   $('.car_img').eq(index).fadeOut(1000).end().eq(indexN).fadeIn(1000);
   // Renouveller le texte
   $('.infos_persos>p').eq(index1).fadeOut(1000).end().eq(indexP).fadeIn(1000);
 
-    // // Renouveller l'image
-    // $('.car_img').eq(index2).fadeOut(1000).end().eq(indexN1).fadeIn(1000);
-    // // Renouveller le texte
-    // $('.infos_persos>p').eq(index3).fadeOut(1000).end().eq(indexP1).fadeIn(1000);
-
   // Mettre à jour l'index
   index = indexN;
   index1 = indexP;
-//   index2 = indexN1;
-//   index3 = indexP1;
+});
+
+$('.fleches').click(function() {
+  // Récupération index
+  const indexN1 = $('.fleches').index(this);
+  const indexP1 = $('.fleches').index(this);
+
+  // Renouveller l'image
+  $('.car_img').eq(index2).fadeOut(1000).end().eq(indexN1).fadeIn(1000);
+  // Renouveller le texte
+  $('.infos_persos>p').eq(index3).fadeOut(1000).end().eq(indexP1).fadeIn(1000);
+
+  // Mettre à jour l'index
+  index2 = indexN1;
+  index3 = indexP1;
 });
 
 // POP UP
